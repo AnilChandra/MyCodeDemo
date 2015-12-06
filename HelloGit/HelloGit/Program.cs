fa1c4e8010be306bafb348c2ad76b259f5a8c278
AnilChandra <chandra_anil@hotmail.com>
2015-12-06 18:53:35 +0530

    More Changes

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -16,6 +16,8 @@ namespace HelloGit
             Console.WriteLine("This is Feature1");
             Console.WriteLine("Additional changes for Feature1");
             Console.WriteLine("This is Feature1.1");
+            Console.WriteLine("Changes after Push/Publish");
+
         }
     }
 }
